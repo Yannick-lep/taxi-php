0fa0e9375927c481daa50557213f151514390661
Yannick-lep <lepinayyannick7@gmail.com>
2025-12-17 16:50:36 +0100

    modif

--- fonctions.php
+++ fonctions.php
@@ -72,3 +72,11 @@ function nettoyer($dataParam) {
     $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
     return $data;
 }
+
+function getNbLigneTable($pdo,$table){
+     $sql = "SELECT COUNT(*) FROM `".$table ."`";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $count= $stmt->fetch();
+        return $last_insert_id;
+}
--- index.php
+++ index.php
@@ -4,3 +4,6 @@ require 'connexiondb.php';
 ?>
 
 <a href="./vehicule/list-vehicule.php">Lister les véhicules</a>
+
+<p>Nombre de véhicule : <?= getNbLigneTable($pdo,'vehicule') ?></p>
+<p>Nombre de conducteurs : <?= getNbLigneTable($pdo,'conducteur') ?></p>
